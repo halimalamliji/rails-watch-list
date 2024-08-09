295cccbd7796cae92e0f6013ba6c7bbd46095d11
Halima Lamliji <halima.lamliji@ymail.com>
2024-08-09 11:04:55 +0200

    rails watch list

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,20 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  resources :lists, only: %i[index new create show] do
+    resources :bookmarks, only: %i[new create]
+  end
+
+  resources :bookmarks, only: %i[destroy]
+
+  #get 'lists', to: 'lists#index'
+  #get 'lists/new', to: 'lists#new', as: 'new_list'
+  #post 'lists', to: 'lists#create'
+
+  #get 'lists/:id', to: 'lists#show', as: 'list'
 end
